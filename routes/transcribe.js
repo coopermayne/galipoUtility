a55d490bc6c1ff3ca3a209a2df924b8c6af3d673
Cooper Mayne <coopermayne@gmail.com>
2023-07-09 21:23:00 -0700

    separated out the processing function

--- routes/transcribe.js
+++ routes/transcribe.js
@@ -36,6 +36,134 @@ const upsertRecord = async (kind, id, data) => {
   }
 };
 
+async function processFile(file, id, initialData) {
+  const bucketName = 'main_bucket3214';
+
+  // Convert audio file to WAV using FFmpeg
+  const convertedFileName = `converted_${file.originalname}_${uuidv4()}.wav`;
+  const convertedFile = storage.bucket(bucketName).file(convertedFileName);
+  await new Promise((resolve, reject) => {
+    ffmpeg(file.path)
+      .audioChannels(1) // Convert to mono (single channel)
+      .toFormat('wav')
+      .output(convertedFile.createWriteStream({ resumable: false }))
+      .on('end', () => {
+        // Remove the original file
+        fs.unlinkSync(file.path);
+        console.log('Conversion completed successfully');
+        resolve();
+      })
+      .on('error', (error) => {
+        // Handle any errors that occur during conversion
+        console.error('Error converting file:', error);
+        reject(error);
+      })
+      .run();
+  });
+  const gcsUri = `gs://${bucketName}/${convertedFileName}`;
+  const httpUrl = `https://storage.googleapis.com/${bucketName}/${convertedFileName}`;
+
+  
+  const secondData = {...initialData, ...{uploadStatus: true, audioFileGs: gcsUri, audioFileHttp: httpUrl}}
+  // Create an entity object with the key and data
+  upsertRecord('transcription', id, secondData)
+  
+  // Start a poll to check if file exists before proceeding to transcription
+  let fileExists = false;
+  let retries = 0;
+  const maxRetries = 20; // set your max retries here
+  const delay = 1000; // delay in milliseconds between each retry
+
+  const checkFileExists = async () => {
+      const [exists] = await storage.bucket(bucketName).file(convertedFileName).exists();
+      fileExists = exists;
+      if (fileExists || retries >= maxRetries) {
+          if (!fileExists) {
+              console.error('Error: File not found in Google Cloud Storage after all retries.');
+              return;
+          }
+          
+          const audio = { uri: gcsUri };
+          const config = {
+            enableAutomaticPunctuation: true,
+            enableWordTimeOffsets: true,
+            languageCode: 'en-US' 
+          };
+
+          const request = { audio: audio, config: config };
+          
+          const [operation] = await client.longRunningRecognize(request);
+          
+          operation
+            .on('complete', async (result, apiResponse) => {
+              const transcription = result.results
+                .map((result) => result.alternatives[0].transcript)
+                .join('\n');
+                
+              // Format the transcription JSON into HTML spans
+              const formatTime = (seconds, nanos) => {
+                const formattedSeconds = seconds || '0';
+                const formattedNanos = nanos || '0';
+                return parseFloat(`${formattedSeconds}.${formattedNanos}`);
+              };
+        
+              const spans = result.results.flatMap((result) =>
+                result.alternatives.flatMap((alternative) => {
+                  const sentenceSpans = alternative.words.map((word) =>
+                    `<span data-start-time="${formatTime(word.startTime.seconds, word.startTime.nanos)}"
+                          data-end-time="${formatTime(word.endTime.seconds, word.endTime.nanos)}">${word.word}</span>`
+                  );
+                  return `<p class="sentence">${sentenceSpans.join(' ')}</p>`;
+                })
+              );
+        
+              const htmlSpans = spans.join(' ');
+        
+              const tdata = {
+                transcriptionText: transcription,
+                transcriptionJson: JSON.stringify(result),
+                transcriptionStatus: true,
+                transcriptionHtml: htmlSpans, // Save the HTML spans to the database
+              };
+        
+              thirdData = {...secondData, ...tdata}
+        
+              await upsertRecord('transcription', id, thirdData)
+            })
+            .on('error', (error) => {
+              console.error(`Error: ${error}`);
+            });
+      } else {
+          retries++;
+          setTimeout(checkFileExists, delay);
+      }
+  }
+
+  checkFileExists();
+}
+
+router.post('/', upload.single('audio'), async (req, res) => {
+  const id = uuidv4();
+
+  const initialData = {
+    id: id,
+    originalFileName: req.file.originalname,
+    newFileName: 'generating...',
+    notes: "testtest notedd",
+    transcriptionStatus: false,
+    uploadStatus: false
+  }
+
+  // Save initial data to the database
+  upsertRecord('transcription', id, initialData)
+
+  // Redirect the user to the view page for this transcription
+  res.redirect(`/transcribe`);
+
+  // Start processing the file asynchronously
+  processFile(req.file, id, initialData);
+});
+
 router.get('/', async (req, res) => {
   try {
     const query = datastore.createQuery('transcription');
@@ -57,52 +185,10 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id;
 
     // Retrieve the transcription from the datastore using the ID
-    const key = datastore.key(['transcription', id]);
-    const [data] = await datastore.get(key);
-    gsUri = data.audioFileLink;
-    const bucketName = gsUri.split('/')[2];
-    const objectName = gsUri.split('/').slice(3).join('/');
-    const audioUrl = `http://storage.googleapis.com/${bucketName}/${objectName}`;
-
-
-    // Get formatted text...
-    let transcriptionJson;
-
-    try {
-      transcriptionJson = JSON.parse(data.transcriptionJson);
-    } catch (error) {
-      res.render('view_transcription', { 
-        textHtml: "", 
-        audioUrl: "",
-        datastore: datastore,
-        title: "Transcription",
-        customRoute: "view_transcription",
-        transcription: data || {} });
-      return;
-    }
-
-    const formatTime = (seconds, nanos) => {
-      const formattedSeconds = seconds || '0';
-      const formattedNanos = nanos || '0';
-      return parseFloat(`${formattedSeconds}.${formattedNanos}`);
-    };
-
-    const spans = transcriptionJson.results.flatMap((result) =>
-      result.alternatives.flatMap((alternative) => {
-        const sentenceSpans = alternative.words.map((word) =>
-          `<span data-start-time="${formatTime(word.startTime.seconds, word.startTime.nanos)}"
-                data-end-time="${formatTime(word.endTime.seconds, word.endTime.nanos)}">${word.word}</span>`
-        );
-        return `<p class="sentence">${sentenceSpans.join(' ')}</p>`;
-      })
-    );
-
-    const html = spans.join(' ');
+    const [data] = await datastore.get(datastore.key(['transcription', id]));
 
     // Render the view_transcription page with the retrieved transcription data
     res.render('view_transcription', { 
-      textHtml: html, 
-      audioUrl: audioUrl,
       datastore: datastore,
       title: "Transcription",
       customRoute: "view_transcription",
@@ -124,128 +210,6 @@ router.post('/:id/delete', async (req, res) => {
     console.error('ERROR:', error);
     res.status(500).send('An error occurred');
   }
-});
-
-router.post('/', upload.single('audio'), async (req, res) => {
-  const io = req.app.get('socketio');
-
-  //===================== create DB record ==================== 
-  const id = uuidv4();
-
-  const initialData = {
-    id: id,
-    originalFileName: req.file.originalname,
-    newFileName: 'generating...',
-    notes: "testtest notedd",
-    transcriptionStatus: false,
-    uploadStatus: false
-  }
-
-  upsertRecord('transcription', id, initialData)
-
-  //===================== file upload ==================== 
-  // const file = req.file;
-  const bucketName = 'main_bucket3214';
-
-  const file = req.file;
-  const fileName = file.originalname;
-
-  // Convert audio file to WAV using FFmpeg
-  const convertedFileName = `converted_${fileName}_${uuidv4()}.wav`;
-  const convertedFile = storage.bucket(bucketName).file(convertedFileName);
-  await new Promise((resolve, reject) => {
-    ffmpeg(file.path)
-      .audioChannels(1) // Convert to mono (single channel)
-      .toFormat('wav')
-      .output(convertedFile.createWriteStream({ resumable: false }))
-      .on('end', () => {
-        // Remove the original file
-        fs.unlinkSync(file.path);
-        console.log('Conversion completed successfully');
-        resolve();
-      })
-      .on('error', (error) => {
-        // Handle any errors that occur during conversion
-        console.error('Error converting file:', error);
-        reject(error);
-      })
-      .run();
-  });
-  const gcsUri = `gs://${bucketName}/${convertedFileName}`;
-  
-  const secondData = {...initialData, ...{uploadStatus: true, audioFileLink: gcsUri}}
-  // Create an entity object with the key and data
-  upsertRecord('transcription', id, secondData)
-  
-  // Start a poll to check if file exists before proceeding to transcription
-  let fileExists = false;
-  let retries = 0;
-  const maxRetries = 20; // set your max retries here
-  const delay = 1000; // delay in milliseconds between each retry
-
-  const checkFileExists = async () => {
-      const [exists] = await storage.bucket(bucketName).file(convertedFileName).exists();
-      fileExists = exists;
-      if (fileExists || retries >= maxRetries) {
-          if (!fileExists) {
-              io.emit('transcribeStatus', 'Error: File not found in Google Cloud Storage after all retries.');
-              return;
-          }
-          io.emit('transcribeStatus', 'Transcription started, please wait...');
-          
-          const audio = { uri: gcsUri };
-          const config = {
-            enableAutomaticPunctuation: true,
-            enableWordTimeOffsets: true,
-            languageCode: 'en-US' 
-          };
-
-          const request = { audio: audio, config: config };
-          
-          const [operation] = await client.longRunningRecognize(request);
-
-          operation
-            .on('progress', (metadata, apiResponse) => {
-              console.log('progress')
-              io.emit('transcribeStatus', `Progress: ${metadata.progressPercent}%`);
-            })
-            .on('complete', (result, apiResponse) => {
-              const transcription = result.results
-                .map((result) => result.alternatives[0].transcript)
-                .join('\n');
-
-              const tdata = {
-                transcriptionText: transcription,
-                transcriptionJson: JSON.stringify(result),
-                transcriptionStatus: true
-              };
-
-              thirdData = {...secondData, ...tdata}
-
-              upsertRecord('transcription', id, thirdData)
-
-              io.emit('transcribeStatus', `Transcription complete!`);
-              io.emit('transcribeResult', JSON.stringify(result));
-            })
-            .on('error', (error) => {
-              io.emit('transcribeStatus', `Error: ${error}`);
-            });
-      } else {
-          retries++;
-          setTimeout(checkFileExists, delay);
-      }
-  }
-
-  checkFileExists();
-
-  const query = datastore.createQuery('transcription');
-  const [transcriptions] = await datastore.runQuery(query);
-  res.render('transcribe', { 
-    transcriptions, 
-    datastore, 
-    title: 'Transcription', 
-    customRoute: 'transcribe'
-  });
 });
 
 module.exports = router;
